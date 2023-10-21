bfec792995520e0b507200f1e47b3954c3394f67
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2023-10-21 20:36:40 +0530

    Update sent.py

--- sent.py
+++ sent.py
@@ -5,7 +5,7 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://login.yahoo.com/?.intl=in&.lang=en-IN&src=ym&activity=header-mail&pspid=2114723002&done=https%3A%2F%2Fin.mail.yahoo.com%2Fd%3F.intl%3Din%26.lang%3Den-IN%26pspid%3D2114723002%26activity%3Dheader-mail&add=1")
 
-input=driver.find_element_by_xpath('//*[@id="login-username"]')
+input=driver.find_element_by_name("username")
 input.send_keys('google') #enter your yahoo mail address
 
 input=driver.find_element_by_xpath('//*[@id="login-signin"]').click()
